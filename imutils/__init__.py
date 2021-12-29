79cf8588a8f9bc704e6e8114907762349d697352
Jacob A Rose <jacobrose@brown.edu>
2021-12-29 08:15:20 -0500

    Initial unit tests set up for robust_image_duplicator. 2 sample jpg images added to image-utils/assets/.

--- imutils/__init__.py
+++ imutils/__init__.py
@@ -2,7 +2,7 @@
 imutils package.
 """
 
-from imutils._imutils import imutils
+from imutils._imutils import ASSETS_DIR, SAMPLE_IMAGE_PATHS, imutils
 
 from .robust_image_duplicator import RobustImageDuplicator
 
@@ -11,5 +11,7 @@ __version__ = "0.0.1"
 __all__ = [
     "imutils",
     "robust_image_duplicator",
+    "ASSETS_DIR",
+    "SAMPLE_IMAGE_PATHS",
     # "RobustImageDuplicator",
 ]
---
+++ tests/unit/test_robust_image_duplicator.py
@@ -0,0 +1,47 @@
+"""
+
+imutils/tests/unit/test_robust_image_duplicator.py
+
+
+"""
+
+import os
+from pathlib import Path
+
+import pandas as pd
+import pytest
+
+from imutils import ASSETS_DIR, SAMPLE_IMAGE_PATHS
+from imutils.robust_image_duplicator import RobustImageDuplicator
+
+
+@pytest.fixture
+def sample_image_paths():
+    return SAMPLE_IMAGE_PATHS
+
+@pytest.fixture
+def sample_catalog(sample_image_paths, tmpdir):
+    assert len(sample_image_paths) == 2
+    assert all([os.path.isfile(path) for path in sample_image_paths])
+    print(f"tmpdir: {tmpdir}")
+    return pd.DataFrame.from_records(
+        [
+            {"source_path": str(sample_image_paths[0]), "target_path": Path(tmpdir) / sample_image_paths[0].name},
+            {"source_path": str(sample_image_paths[1]), "target_path": Path(tmpdir) / sample_image_paths[1].name},
+        ]
+    )
+
+
+def test_sample_catalog(sample_catalog):
+
+    assert sample_catalog.source_path.apply(os.path.isfile).all()
+    assert (~sample_catalog.target_path.apply(os.path.isfile)).all()
+    print(sample_catalog)
+
+
+def test_RobustImageDuplicator_init(sample_catalog):
+
+    image_duplicator = RobustImageDuplicator(data=sample_catalog, log_every=1, use_swifter=False)
+    correct, incorrect = image_duplicator.run()
+    assert sample_catalog.target_path.apply(os.path.isfile).all()
+    print(f"# correct: {correct}, # incorrect: {incorrect}")

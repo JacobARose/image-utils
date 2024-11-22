9223fef2c5dd05e2fab879527e54ff68b6e9b332
Jacob A Rose <31635761+JacobARose@users.noreply.github.com>
2024-11-22 00:59:44 -0500

    Update __init__.py -- commented out the absurd `from imutils._imutils import imutils`
    what was i thinking

--- imutils/__init__.py
+++ imutils/__init__.py
@@ -2,7 +2,7 @@
 imutils package.
 """
 
-from imutils._imutils import ASSETS_DIR, SAMPLE_IMAGE_PATHS, imutils
+from imutils._imutils import ASSETS_DIR, SAMPLE_IMAGE_PATHS #, imutils
 
 from .robust_image_duplicator import RobustImageDuplicator
 from .utils import ResizeRight
